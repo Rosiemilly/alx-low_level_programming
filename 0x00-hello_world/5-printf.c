04ec8d30df0c409a8099714b71c914e58602dfda
Rosiemilly <rosiemilly490@gmail.com>
2023-06-17 01:39:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that printa line using the printf function 
+ * Return: 0 (Done)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+reture(0);
+}
