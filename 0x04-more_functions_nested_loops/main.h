a7ed8b7775cf1585f8006c3f6caddf308f963bf9
Rosiemilly <rosiemilly490@gmail.com>
2023-06-22 02:33:13 -0700

    loop is hard

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,16 @@
+#ifinde MAIN_H
+#define  MAIN_H
+
+int -putchar(char c);
+int -isupper(int c);
+int -isdigit(int c);
+int mul(int a,int b);
+void print_numbers(void);
+void print _most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
